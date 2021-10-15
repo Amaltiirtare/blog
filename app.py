429b08bc9c03d32340314f88330385148680c425
aest <aest@ya.ru>
2021-10-15 14:48:41 +0700

    Create header and footer

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+# Для установки времени по умолчанию
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+
+# nullable=False - нельзя установить пустое значение
+# Text - для большого объема текста
+# default= -  значение по умолчанию
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+    # Из БД выдается объект и ее ID
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
